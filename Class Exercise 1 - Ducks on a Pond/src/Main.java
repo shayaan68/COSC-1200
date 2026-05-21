03f93a26cfbf49c0db9d3bc4f40633d614bd0978
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-21 12:59:57 -0400

    Just trying to structure the code
    To be honest, I do think I'm going about this like its python way too much.

--- Class Exercise 1 - Ducks on a Pond/src/Main.java
+++ Class Exercise 1 - Ducks on a Pond/src/Main.java
@@ -1,17 +1,29 @@
 // Name: Shayaan Shah
 // Date: May 21st 2026
 // File Name: Class Exercise 1: Ducks on a Pond
-// Description: Create a Java program that determines the circumriance of how much we will need
+// Description: Create a Java program that determines the circumriance of how many duckls would fit in 2 pools of an equal size
 
-public class Main {
+import java.util.Scanner;
+public class Main {  
     public static void main(String[] args) throws Exception {
-        
+        Scanner Scanner = new Scanner(System.in);
         //decleartion
 
         int poolLength;
         int poolWidth;
         int duckArea = 6;
+        int poolArea = poolLength * poolWidth;
         
-        System.out.println("Hi! Welcome to the Duck Pool Caculator");
+        System.out.print("Hi! Welcome to the Duck Pool Caculator");
+           
+        System.out.print("Please enter a length for the pool "); 
+            int poolLength = Scanner.nextInt();
+
+        System.out.print("Please enter a Width for the pool "); 
+            int poolWidth = Scanner.nextInt();
+
+        
+            
+        
     }
 }
