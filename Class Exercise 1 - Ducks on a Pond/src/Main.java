6f2b1595d367aa73ae78cb25a10bf8ec88c5d2c9
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-21 14:02:39 -0400

    Just made a few comment changes
    Couldnt finish the validation but least it acutally post

--- Class Exercise 1 - Ducks on a Pond/src/Main.java
+++ Class Exercise 1 - Ducks on a Pond/src/Main.java
@@ -1,37 +1,38 @@
 // Name: Shayaan Shah
 // Date: May 21st 2026
 // File Name: Class Exercise 1: Ducks on a Pond
-// Description: Create a Java program that determines the circumriance of how many duckls would fit in 2 pools of an equal size
+// Description: Create a Java program that determines the amount of rubber ducks  that would fit in one pools of an area that the user sets
 
 import java.util.Scanner;
 public class Main {  
     public static void main(String[] args) throws Exception {
         Scanner Scanner = new Scanner(System.in);
-        //decleartion
+  
 
-        
+        //user input
         System.out.println("Hi! Welcome to the Duck Pool Caculator");
            
         System.out.print("Please enter a Length for the pool in Meters "); 
             double poolLength = Scanner.nextInt();
+            
 
         System.out.print("Please enter a Width for the pool in Meters "); 
             double poolWidth = Scanner.nextInt();
-
         
         
+        //the duck is 25cm worth of area
+        double duckArea = 0.25; 
         
+        //this is for cacualting the measuments of the pool
+        double totalPoolArea = poolLength * poolWidth; 
         
-        
-        
-        
-        double duckArea = 0.25; //the duck is 25cm worth of area
-        double totalPoolArea = poolLength * poolWidth; //this is for cacualting the measuments of the pool
+        //caculating the amount of ducks that can fit in the pools area via dividing the pool area by 1 ducks surface area (25cm)
         double totalPoolDucks = totalPoolArea / duckArea;
 
         Scanner.close();
         
-        System.out.print("The "totalPoolDucks); 
+        System.out.printf("The total amount of ducks that can fit in a %.2f", totalPoolArea);
+        System.out.printf(" m pool is %.2f ", totalPoolDucks); 
         
     }
 }
