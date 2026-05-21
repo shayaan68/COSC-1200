d561269755139ece8676d01a5a898ed7741a3468
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-21 12:49:24 -0400

    Initial Int
    Just setting up the project

---
+++ Class Exercise 1 - Ducks on a Pond/src/Main.java
@@ -0,0 +1,17 @@
+// Name: Shayaan Shah
+// Date: May 21st 2026
+// File Name: Class Exercise 1: Ducks on a Pond
+// Description: Create a Java program that determines the circumriance of how much we will need
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        //decleartion
+
+        int poolLength;
+        int poolWidth;
+        int duckArea = 6;
+        
+        System.out.println("Hi! Welcome to the Duck Pool Caculator");
+    }
+}
