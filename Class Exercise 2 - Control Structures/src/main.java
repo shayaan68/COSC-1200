8b66ac3bc614ab6d18e97053ae826d3444a27b6d
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-27 12:07:51 -0400

    (sort of) setting up the validation
    Ill probably need to ask about if the range is left up to us or if their is an assigned one (I didn't see any assigned range in the rubric).

--- Class Exercise 2 - Control Structures/src/main.java
+++ Class Exercise 2 - Control Structures/src/main.java
@@ -9,14 +9,26 @@ public class main {
     public static void main(String[] args) throws Exception {
         Scanner Scanner = new Scanner(System.in);
 
-        //user input
-
+        //user input and validation 
         System.out.println("Hello, Welcome to the number guesser!");
-
         System.out.print("Please enter a guess for the number that was randomly selected"); 
             double userSelectedNumber = Scanner.nextInt();
 
+            if (userSelectedNumber >= 10 && userSelectedNumber >= 0); {
 
+                System.out.println("This is to see if the if else statments work");
+
+        } 
+            else{
+              System.out.println("Error! Please enter a number in between or equal to 1 or 10");
+            }
+
+        
+        
+
+        
+        
+        
     
         
     }
