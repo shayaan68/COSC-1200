bfcd70d367c4d0523bd6cd0d20564844c0ba0502
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-27 13:39:27 -0400

    ok bruh how did that even work?
    Imma be honest, I have no idea why scanner.nextInt somewhat even worked to begin with as when I put userSelectedNumber scanner.next as a double instead of an int, the decimal error statement actually works better than the valid and the other invalid statement as it doesn't loop itself endlessly. If I don't figure out why by today Ill ask during tomorrows class.

--- Class Exercise 2 - Control Structures/src/main.java
+++ Class Exercise 2 - Control Structures/src/main.java
@@ -12,24 +12,25 @@ public class main {
         //user input and validation 
         System.out.println("Hello, Welcome to the number guesser!");
         System.out.print("Please enter a guess for the number that was randomly selected"); 
-            double userSelectedNumber = Scanner.nextInt();
+            double userSelectedNumber = Scanner.nextDouble();
+            
+            
             while (userSelectedNumber % 1 == 0){
                 
                 if (userSelectedNumber <= 10 && userSelectedNumber >= 0) {
                     System.out.println("Ok the vaidation works");
-                    Scanner.close(); 
-                
-                
-                
-                
-                
+                     
+    
                 }else {
-                    System.out.println("Error!: Please Enter a positive whole number in the range of 0-10");
+                    System.out.println("Error!: Please enter a POSITIVE NUMBER in the range of 0-10");
+                    
                 }  
+            } 
+                System.out.println("Error!: Please Enter a POSITIVE WHOLE NUMBER in the range of 0-10");
             }
     
         }
-    }
+
 
 
          
