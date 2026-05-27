1452cdd13fe6c069aca5e6eed2b62d3f573c9f1d
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-27 12:35:12 -0400

    Realised we need to use while loops
    It honestly worked out for the better as I can just check before generating the number to be guessed if the numeric user input is a whole number by using the % division to see if their is any other remainder output other than 0. As a decimal input will never have a remainder of 0. Then if that comes out as true, I can check for negatives (as a negative whole number divide by 1 will have 0 remainder meaning invalid input could pass as valid) by seeing if the user numeric value is more than or less than 0 or 10 which if that the case catches the false user input not on being a decimal but on not being a whole number in the range of only valid whole numbers which are not negatives.

--- Class Exercise 2 - Control Structures/src/main.java
+++ Class Exercise 2 - Control Structures/src/main.java
@@ -13,23 +13,19 @@ public class main {
         System.out.println("Hello, Welcome to the number guesser!");
         System.out.print("Please enter a guess for the number that was randomly selected"); 
             double userSelectedNumber = Scanner.nextInt();
-
-            if (userSelectedNumber >= 10 && userSelectedNumber >= 0); {
-
-                System.out.println("This is to see if the if else statments work");
+            
+            while (userSelectedNumber % 1 == 0)
+                
+                if (userSelectedNumber >= 10 && userSelectedNumber >= 0); {
+                   
+    
+                else {
+                    
+                }
 
         } 
-            else{
-              System.out.println("Error! Please enter a number in between or equal to 1 or 10");
-            }
-
-        
-        
-
-        
-        
-        
-    
+             
+        }
         
     }
-}
+
