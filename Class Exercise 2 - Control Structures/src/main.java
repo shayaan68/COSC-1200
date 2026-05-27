6f6389b9f1c2af1b622d5868e030fb5d26a31fdb
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-27 11:50:33 -0400

    Put the genral information and a rought versio of the user input
    I know my previous CE biggest criticism was my lack of error statements should invalid input be entered and the bad origination of my code not being to the course stylization. I've only organised it in the same way as before solely for my own convince while working on this and will rework it to be up to the courses stylization stranded before submitting

---
+++ Class Exercise 2 - Control Structures/src/main.java
@@ -0,0 +1,23 @@
+// Name: Shayaan Shah
+// Date: May 27st 2026
+// File Name: Class Exercise 2: NumberGuessing
+// Description: Create a Java program that preselects a random number then asks the user for input to guess it.
+
+
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) throws Exception {
+        Scanner Scanner = new Scanner(System.in);
+
+        //user input
+
+        System.out.println("Hello, Welcome to the number guesser!");
+
+        System.out.print("Please enter a guess for the number that was randomly selected"); 
+            double userSelectedNumber = Scanner.nextInt();
+
+
+    
+        
+    }
+}
