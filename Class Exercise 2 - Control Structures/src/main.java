74100dce10c80303075086e3a4e74471d8f19f6c
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-28 17:56:32 -0400

    Also if I didnt say it already then sightly modification to the change of plans
    I didn't know how to tick all the boxes for all the if statements need for the only other possible way it could fail all of them would leave it with the same amount of the genratedNumberAnswer. So instead, I made the very last else if for the correct input and answer match and the else for any input that dosent match anything for the if statements (so like letters and symbols.)

--- Class Exercise 2 - Control Structures/src/main.java
+++ Class Exercise 2 - Control Structures/src/main.java
@@ -15,55 +15,53 @@ public class main {
         int generatedNumberAnswer = (int) (Math.random() * 100)  + 1;
         
         int userSelectedNumber = 0;
-            while (userSelectedNumber != generatedNumberAnswer){ //This is to set the loop that while the user genrated number and the number that was generated are not he same, the code will loop itself till it is. 
+            //This is to set the loop that while the user genrated number and the number that was generated are not the same, 
+            // the code will loop itself till it is. It also will be easier to count how many times this statement held true so that the attempts can be counted easier.        
+            while (userSelectedNumber != generatedNumberAnswer){ 
                 userSelectedNumber = Scanner.nextInt();{
+                    
+                    //Error vaildation to check for both negitives, and invaild range 
                     if (userSelectedNumber < 1 || userSelectedNumber > 100) {
                         System.out.println("ERROR:Please enter a Numeric whole number in  the range of 1-100");
+                    
+                    
+                    //the first 2 else if are for if the user inputed vailded input 
+                    // but did not have the same answer as the genratred number which then thy will be prompted to try again
                     }else if (userSelectedNumber > generatedNumberAnswer) {
-                            System.out.print("You guessed: ");
-                            System.out.print(userSelectedNumber);
-                            System.out.print("and the answer was: ");
-                            System.out.print(generatedNumberAnswer);
-                            System.out.print(" meaning that your answer was too high");
-                        }else if (userSelectedNumber < generatedNumberAnswer) {
-                            System.out.print("You guessed: ");
-                            System.out.print(userSelectedNumber);
-                            System.out.print(" and the answer was: ");
-                            System.out.print(generatedNumberAnswer);
-                            System.out.print(" meaning that your answer was too low");
+                        System.out.print("You guessed: ");
+                        System.out.print(userSelectedNumber);
+                        System.out.print("you were incorrect since your answer was too high, Try guessing again!: ");
+                    }else if (userSelectedNumber < generatedNumberAnswer) {
+                        System.out.print("You guessed: ");
+                        System.out.print(userSelectedNumber);
+                        System.out.print(" you were incorrect since your answer was too low Try guessing again!:");
 
-                        }else if (userSelectedNumber == generatedNumberAnswer) {
-                                System.out.print("You guessed: ");
-                                System.out.print(userSelectedNumber);
-                                System.out.print("and the answer was: ");
-                                System.out.print(generatedNumberAnswer);
-                                System.out.print("That means you are correct!");
-                                System.out.print("And it only took you");
-                        }else {
-                                System.out.print("You inputted invaild input, please enter a postiive whole numer in the range of 1-100");
-                                
-                            
-
-                        }   
+                        //where the user input would land if its both vaild and the same amount as the genratedNumberAnswer
+                    }else if (userSelectedNumber == generatedNumberAnswer) {
+                        System.out.print("You guessed: ");
+                        System.out.print(userSelectedNumber);
+                        System.out.print("and the answer was: ");
+                        System.out.print(generatedNumberAnswer);
+                        System.out.print("That means you are correct!");
+                        System.out.print("And it only took you");
+                        Scanner.close();
+                    }else {
+                       Scanner.hasNextInt();
+                        System.out.print("You inputted invaild input, please enter a postiive whole numer in the range of 1-100");
+                    }   
+                        }
+                    } 
+                }  
+                }
                 
-
-                            }
-                    
-
-                        } 
-                    }  
-                    
-                    
-                    }
-                    
-                
+            
+            
+        
                 
             
                     
-                
-                       
 
 
-         
- 
+        
+
 
