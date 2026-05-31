52d1ac49b1f31c50a795e27bec3415e6ffc7e690
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-05-31 18:17:49 -0400

    Inital int
    Starting the projectile motion assignment. This time I'm planning the whiles and validation out from the start without changing my mind halfway through

---
+++ Assignment 1 - Projectile Motion/Assignment 1 - Projectile Motion/src/projectileMotionCaculator.java
@@ -0,0 +1,9 @@
+// Name: Shayaan Shah
+// Date: May 31st 2026
+// File Name: Assignment 1 - Projectiles Motion
+// Description: 
+public class projectileMotionCaculator {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
