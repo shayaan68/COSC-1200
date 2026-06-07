e5fc0233a1a0441511a55413f3fc3a1058f986e4
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-06-07 00:03:24 -0400

    Actually starting up this
    Ok I genuinely meant to start this earlier last week . but I got so ill that I got knocked out for almost two days straight. I have been reading the feedback on the in classes so Ill think Ill be able to avoid the hicups I had with them, especuially the 2nd inclass.

--- Assignment 1 - Projectile Motion/Assignment 1 - Projectile Motion/src/projectileMotionCaculator.java
+++ Assignment 1 - Projectile Motion/Assignment 1 - Projectile Motion/src/projectileMotionCaculator.java
@@ -2,8 +2,17 @@
 // Date: May 31st 2026
 // File Name: Assignment 1 - Projectiles Motion
 // Description: 
-public class projectileMotionCaculator {
+import java.util.Scanner;
+public class ProjectileMotionCaculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         //user input
+        System.out.println("Hi! Welcome to the Projectile Motion caculator!");
+           
+        System.out.print("Please enter the angle of the projectile in degrees"); 
+            double Degrees = Scanner.nextInt();
+        
+        System.out.print("Please enter the inital velocity of the projectile im m/s "); 
+            double InitialVelocity = Scanner.nextInt();
+    
     }
 }
