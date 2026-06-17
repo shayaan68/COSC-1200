5fc315a50e51e9930dfe212cacaa2713ceb8a7a3
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-06-17 12:59:54 -0400

    Adding the variable delcrations and comments
    The varabile comments are temporary so that I don't have to constantly alt tab

--- Class Exercise 3 - Functions/NumberTools/src/NumberTools.java
+++ Class Exercise 3 - Functions/NumberTools/src/NumberTools.java
@@ -1,5 +1,33 @@
+// Name: Shayaan Shah
+// Date: June 17th 2026
+// File Name: Class - Projectiles Motion
+// Description: Creating a class that does all the caculations and then importing that class in another Java class file that displays the user input and the output of the caculations of the first java file
+
+import java.util.Scanner;
 public class NumberTools {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Declaring the variables for each of the caculations
+        int isEven;
+        //isEven(int value): A function that will return true 
+        // if its parameter is evenly divisible by 2, 
+        // and return false otherwise.
+        
+        int isSingleDigit;
+        //isSingleDigit(int value): A function that will return true 
+        // if its parameter is between -9 and 9, 
+        // and return false otherwise.
+        
+        int isPowerOfTwo;
+        //isPowerOfTwo(int value): A function that will return true 
+        // if its parameter is a power of 2 (e.g. 2, 4, 8, 16, 32…), and return false otherwise. 
+        // (You could also genericize it into isPower() 
+        // to work with any base value!)
+        
+        int isMultipleOfTen;
+        //isMultipleOfTen(int value): A function that will return true 
+        // if its parameter is a multiple of 10 (e.g. 10, 20, 40, …), and return false otherwise. 
+        // (You could also genericize it into isMultiple() to work with any multiple!)
+
     }
 }
