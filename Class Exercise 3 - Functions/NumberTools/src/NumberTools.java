243389f8b3aaf8b38e1441cf9a7cf46d366f134e
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-06-17 17:35:38 -0400

    Reworked a few thing plus changed the 3rd problem
    Yeah I couldn't figure out the third problem to save my life so I changed it. Also I realised that I had my whiles and if conditions the wrong way around. Also, Validation could be handled on the NumberTesting class and not Numbertools as we are olny giving outcomes for valid input

--- Class Exercise 3 - Functions/NumberTools/src/NumberTools.java
+++ Class Exercise 3 - Functions/NumberTools/src/NumberTools.java
@@ -2,27 +2,28 @@
 // Date: June 17th 2026
 // File Name: Class - Projectiles Motion
 // Description: Creating a class that does all the caculations and then importing that class in another Java class file that displays the user input and the output of the caculations of the first java file
-
 import java.util.Scanner;
 public class NumberTools {
     public static void main(String[] args) throws Exception {
         
         //Declaring the variables for each of the caculations
         int isEven = 0;
-        //isEven(int value): A function that will return true 
+        //isEven(int value): 
+        // A function that will return true 
         // if its parameter is evenly divisible by 2, 
         // and return false otherwise.
         
         int isSingleDigit = 0; 
-        //isSingleDigit(int value): A function that will return true 
+        //isSingleDigit(int value): 
+        // A function that will return true 
         // if its parameter is between -9 and 9, 
         // and return false otherwise.
         
-        int isPowerOfTwo;
-        //isPowerOfTwo(int value): A function that will return true 
-        // if its parameter is a power of 2 (e.g. 2, 4, 8, 16, 32…), and return false otherwise. 
-        // (You could also genericize it into isPower() 
-        // to work with any base value!)
+        int isSquare = 0;
+        //isSquare(int value): 
+        // A function that will return true if its parameter is a square number 
+        // (e.g. the result of an integer squared, or has an integer square root), 
+        // and return false otherwise.
         
         int isMultipleOfTen;
         //isMultipleOfTen(int value): A function that will return true 
@@ -30,31 +31,21 @@ public class NumberTools {
         // (You could also genericize it into isMultiple() to work with any multiple!)
 
 
-        while (isEven % 2 == 0) {
-            System.out.print("Your inputed number was a even number!");
-            if (isEven % 2 != 0) {
-                System.out.print("Your inputed number was an odd number!");
+        while (isEven % 2 != 0) {
+            if (isEven % 2 == 0) {
+            System.out.println("YES! Your inputed number was a even number!");
             } else {
-                System.out.print("Error! please enter a whole number");}
+                System.out.println("NO! Your inputed number was an odd number!");
 
         while (isSingleDigit >= -9 && isSingleDigit <=9); {
-            System.out.print("Your inputed number was in between -9 and 9!");
+            System.out.println("YES! Your inputed number was in between -9 and 9!");
             if (isSingleDigit < -9 || isSingleDigit > 9) {
-                System.out.print("Your inputed number was an odd number!");
+                System.out.println("NO! Your inputed number was an odd number!");
             } else {
-                System.out.print("Error! please enter a whole number");}
-        
-        while (isPowerOfTwo % 2 == 0 ); {
-            System.out.print("Your inputed number was a square root of 2!");
-            if (isPowerOfTwo % 2 !=0 ) {
-                System.out.print("Your inputed number was not a power!");
-            } else {
-                System.out.print("Error! please enter a whole number");}
-        
-        
-
+                System.out.println("Error! please enter a whole number");}
         }
         }
         }
         }
+    }
 
