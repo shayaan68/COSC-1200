52d63fb943e531e6e7e4c8f28517ecc4b4854905
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-07-14 21:55:11 -0400

    Big change Of when I call my objects
    Im kinda a doufus for doing that in my getImpact class when I could just do it in runGame which is the main class anyways

--- Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/getImpact.java
+++ Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/getImpact.java
@@ -3,33 +3,20 @@ public class getImpact {
     
     
     public double finalImpact;
-    
-    public void caculateImpact(Scanner Scan){
-        
-        double time = 0;
-        double timeIncrease = 1;
-        boolean impactVerification = false;
-        double yAxis = 0;
-        double xAxis = 0;
-        
-        //This is recalling the two classes and turning the objects into doubles that Ill need for get impact
-        getPower velocityPower = new getPower();
-        velocityPower.caculatePower(Scan);
-        double Velocity = velocityPower.getPowerCaculations();
 
-        getAngle thrownAngle = new getAngle();
-        thrownAngle.caculateAngle(Scan);
-        double Angle = thrownAngle.getCaculateAngle();
+    double yAxis = 0;
+    double xAxis = 0;
+    double time = 0;
+    double timeIncrease = 1;
+    double finalTime;
+    boolean impactVerification = false;
     
-    
+    public void caculateImpact(double Velocity, double Angle){
+        
         time += timeIncrease;
         yAxis = ((Velocity * Math.sin(Math.toRadians(Angle)) * time - (9.81 * time*time)/2));
-        xAxis = (Velocity * Math.sin(Math.toRadians(Angle))*time);
-            
-        double FinalTime = (2 * Velocity * Math.sin(Math.toRadians(Angle))) / 9.81;
-
-
-        
+        xAxis = (Velocity * Math.cos(Math.toRadians(Angle))*time);
+        finalTime = (2 * Velocity * Math.sin(Math.toRadians(Angle))) / 9.81;
         }
     }
 
--- Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/runGame.java
+++ Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/runGame.java
@@ -1,26 +1,51 @@
-
+import java.util.Scanner;
+public class runGame {
 public static void main(String[] args) {
-
+    Scanner Scan = new Scanner(System.in);
     //Declaring the variable to store the distance of each of the players throw and their names
     double playerOneThrow;
     double playerTwoThrow;
-    int playerOneName;
-    int playerTwoName;
-    boolean nameVerification = false;
+    boolean gameVerification = false;
     
-    
-    
-    
-    while (!nameVerification) {
     
     System.out.println("Hi! Welcome to the Projectile Motion Mulitplayer Game!");
-    System.out.println("Please Enter the First Players Name");
-    playerOneName = scan.nextInt;
+    //I was going to do a vaild input check, but to be honest it dodnt say if you could or couldnt use any other charcters
+    //so I decided to scrap it it
+    System.out.println("Please Enter the 1st Players Name");
+    String playerOneName = Scan.nextLine();
+    System.out.println("Please Enter the 2nd Players Name");
+    String playerTwoName = Scan.nextLine();
+    while (!gameVerification) {
+    
+
+        //Doing the complete caculation for the first player then moving on to the 2nd 
+        // as my angle and power classes are only deigned to check for vaild input for one input at a time
+        //doing both play 1 and 2s speed at the same time caused more problems
+        getPower velocityPower = new getPower();
+        System.out.printf("Please enter the speed for", playerOneName);
+        velocityPower.caculatePower(Scan);
+        double Velocity = velocityPower.getPowerCaculations();
 
 
+        getAngle thrownAngle = new getAngle();
+        System.out.printf("Please enter the angle thrown at a minium of 0 and a maximum of 180 degrees for", playerOneName);
+        thrownAngle.caculateAngle(Scan);
+        double Angle = thrownAngle.getCaculateAngle();
 
+
+        //Now calling on get impact 
+        getImpact impact = new getImpact();
+        impact.caculateImpact(Velocity, Angle);
+        
+        
+
+        
+        
+        
+    }
     
     }
 }
+
 
 
