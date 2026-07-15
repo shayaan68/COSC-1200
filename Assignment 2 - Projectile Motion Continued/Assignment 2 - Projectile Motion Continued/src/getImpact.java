bcebed540b37de8a26ca6adc7a79aa575e1e61de
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-07-15 02:31:13 -0400

    redid the variables that called on the other classes
    Also am finally finishing up the getImpact, in hindsight, reusing my previous code as a starting point was not a good idea

--- Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/getImpact.java
+++ Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/getImpact.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
 public class getImpact {
     
     
     public double finalImpact;
-
-    double yAxis = 0;
-    double xAxis = 0;
-    double time = 0;
-    double timeIncrease = 1;
-    double finalTime;
-    boolean impactVerification = false;
     
     public void caculateImpact(double Velocity, double Angle){
+        
+        double yAxis = 0;
+        double xAxis = 0;
+        double time = 0;
+        double timeIncrease = 1;
+        double finalTime;
+        
         
         time += timeIncrease;
         yAxis = ((Velocity * Math.sin(Math.toRadians(Angle)) * time - (9.81 * time*time)/2));
--- Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/runGame.java
+++ Assignment 2 - Projectile Motion Continued/Assignment 2 - Projectile Motion Continued/src/runGame.java
@@ -7,35 +7,54 @@ public static void main(String[] args) {
     double playerTwoThrow;
     boolean gameVerification = false;
     
+
     
-    System.out.println("Hi! Welcome to the Projectile Motion Mulitplayer Game!");
-    //I was going to do a vaild input check, but to be honest it dodnt say if you could or couldnt use any other charcters
-    //so I decided to scrap it it
-    System.out.println("Please Enter the 1st Players Name");
-    String playerOneName = Scan.nextLine();
-    System.out.println("Please Enter the 2nd Players Name");
-    String playerTwoName = Scan.nextLine();
     while (!gameVerification) {
     
-
+        System.out.println("Hi! Welcome to the Projectile Motion Mulitplayer Game!");
+        //I was going to do a vaild input check, but to be honest it dodnt say if you could or couldnt use any other charcters
+        //so I decided to scrap it it
+        System.out.println("Please Enter the 1st Players Name");
+        String playerOneName = Scan.nextLine();
+        System.out.println("Please Enter the 2nd Players Name");
+        String playerTwoName = Scan.nextLine();
+    
         //Doing the complete caculation for the first player then moving on to the 2nd 
         // as my angle and power classes are only deigned to check for vaild input for one input at a time
         //doing both play 1 and 2s speed at the same time caused more problems
-        getPower velocityPower = new getPower();
-        System.out.printf("Please enter the speed for", playerOneName);
-        velocityPower.caculatePower(Scan);
-        double Velocity = velocityPower.getPowerCaculations();
+        
+        //Entering the first players speed and angle
+        getPower playerOneVelocity= new getPower();
+        System.out.println("Please enter the speed for " + playerOneName+  ":");
+        playerOneVelocity.caculatePower(Scan);
+        double velocity1 = playerOneVelocity.getPowerCaculations();
+
+        
+        getAngle playerOneAngle = new getAngle();
+        System.out.println("Please enter the angle thrown at a minium of 0 and a maximum of 180 degrees for " + playerOneName +":");
+        playerOneAngle.caculateAngle(Scan);
+        double angle1 = playerOneAngle.getCaculateAngle();
+
+        
+        //Entering the secaond players speed and angle
+        getPower playerTwoVelocity = new getPower();
+        System.out.println("Please enter the speed for" + playerTwoName +":");
+        playerTwoVelocity.caculatePower(Scan);
+        double velocity2 = playerTwoVelocity.getPowerCaculations();
 
 
-        getAngle thrownAngle = new getAngle();
-        System.out.printf("Please enter the angle thrown at a minium of 0 and a maximum of 180 degrees for", playerOneName);
-        thrownAngle.caculateAngle(Scan);
-        double Angle = thrownAngle.getCaculateAngle();
+        getAngle playerTwoAngle = new getAngle();
+        System.out.println("Please enter the angle thrown at a minium of 0 and a maximum of 180 degrees for "+ playerTwoName+ ":");
+        playerTwoAngle .caculateAngle(Scan);
+        double angle2 = playerTwoAngle.getCaculateAngle();
 
 
         //Now calling on get impact 
-        getImpact impact = new getImpact();
-        impact.caculateImpact(Velocity, Angle);
+        getImpact playerOneImpact = new getImpact();
+        playerOneImpact.caculateImpact(velocity1, angle1);
+        
+        getImpact playerTwoImpact = new getImpact();
+        playerTwoImpact.caculateImpact(velocity2, angle2);
         
         
 
