8a9b623cb9bb19597b1ec2381a1c0772c3bf0a3e
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-08-09 20:09:05 -0400

    setting up the runRounds class (game class)
    Im essntialy setting up the round class to be the simulation of the averge round while making it so that once it ends, it refreshes the skips turn for all players

---
+++ Project User Interface - Russian Roulete/src/runRounds.java
@@ -0,0 +1,21 @@
+// Name: Shayaan Shah
+// Date: Aug 6th 2026
+// File Name: Project User Interface - Russian Roulette
+// Description: 
+import java.util.Scanner;
+
+
+public class runRounds {
+    
+    private int currentPlayer;
+    private int currentRound;
+    private int[] skipTurnLeft;
+    
+    public runRounds() {
+        currentPlayer = 0;
+        currentRound = 1;
+    }
+
+
+
+}
