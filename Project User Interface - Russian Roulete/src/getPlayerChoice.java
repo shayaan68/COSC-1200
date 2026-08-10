72047907638326b1eba8cf7a873305e18aef937f
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-08-09 22:32:30 -0400

    player choice now done

--- Project User Interface - Russian Roulete/src/getPlayerChoice.java
+++ Project User Interface - Russian Roulete/src/getPlayerChoice.java
@@ -12,11 +12,24 @@ public class getPlayerChoice {
         int playerChoice = 0;
 
         while (!choiceVerification) {
-             System.out.println("What would you like to do?");
+            System.out.println("What would you like to do?");
             System.out.println("1. Pull Trigger");
             System.out.println("2. Skip");
+            
+            if (Scan.hasNextInt()) {
+                playerChoice = Scan.nextInt();
+
+                if (playerChoice == 1 || playerChoice == 2) {
+                    choiceVerification= true;
+                } else {
+                    System.out.println("Invaild Input: Enter 1 or 2");
+                }
+            } else {
+                System.out.print("Invaild Input: Enter 1 or 2");
+            }
 
         }
+        return playerChoice;
     }
     
 }
