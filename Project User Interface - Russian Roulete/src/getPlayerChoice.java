c2163545a01af166696170aef7c58ac57564714c
shayaan68 <158122854+shayaan68@users.noreply.github.com>
2026-08-09 22:20:32 -0400

    setrting up the player choice class delecraiations and the print out options to display

--- Project User Interface - Russian Roulete/src/getPlayerChoice.java
+++ Project User Interface - Russian Roulete/src/getPlayerChoice.java
@@ -1,3 +1,22 @@
+// Name: Shayaan Shah
+// Date: Aug 6th 2026
+// File Name: Project User Interface - Russian Roulette
+// Description: 
+import java.util.Scanner;
+
 public class getPlayerChoice {
+
+    public int getChoice(Scanner Scan) {
+        
+        boolean choiceVerification = false;
+        int playerChoice = 0;
+
+        while (!choiceVerification) {
+             System.out.println("What would you like to do?");
+            System.out.println("1. Pull Trigger");
+            System.out.println("2. Skip");
+
+        }
+    }
     
 }
